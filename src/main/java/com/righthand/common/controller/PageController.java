c6312f644690d061ddcf50d41cb96f2337bc6897
SML <dltkdals2202@gmail.com>
2018-10-29 17:09:44 +0900

    style

--- src/main/java/com/righthand/common/controller/PageController.java
+++ src/main/java/com/righthand/common/controller/PageController.java
@@ -1,6 +1,8 @@
 package com.righthand.common.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,25 +18,42 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class PageController {
 
-    @RequestMapping("/")
-    public String index(HttpServletRequest request) {
-        return "/index.html";
+    @ApiOperation(value = "로그인")
+    @GetMapping("/login")
+    public String customLogin(){
+        return "login";
     }
 
-    @RequestMapping("/page/{depth1}")
-    public String depth1(HttpServletRequest request, @PathVariable String depth1) {
-        return depth1;
+    @GetMapping("/user")
+    public String user(){
+        return "user";
     }
 
-    @RequestMapping("/page/{depth1}/{depth2}")
-    public String depth2(HttpServletRequest request, @PathVariable String depth1, @PathVariable String depth2){
-        return depth1+"/"+depth2;
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
     }
 
-    @RequestMapping("/page/{depth1}/{depth2}/{depth3}")
-    public String depth3(HttpServletRequest request, @PathVariable String depth1, @PathVariable String depth2, @PathVariable String depth3) {
-        return depth1+"/"+depth2+"/"+depth3;
-    }
+
+//    @RequestMapping("/")
+//    public String index(HttpServletRequest request) {
+//        return "/index.html";
+//    }
+//
+//    @RequestMapping("/page/{depth1}")
+//    public String depth1(HttpServletRequest request, @PathVariable String depth1) {
+//        return depth1;
+//    }
+//
+//    @RequestMapping("/page/{depth1}/{depth2}")
+//    public String depth2(HttpServletRequest request, @PathVariable String depth1, @PathVariable String depth2){
+//        return depth1+"/"+depth2;
+//    }
+//
+//    @RequestMapping("/page/{depth1}/{depth2}/{depth3}")
+//    public String depth3(HttpServletRequest request, @PathVariable String depth1, @PathVariable String depth2, @PathVariable String depth3) {
+//        return depth1+"/"+depth2+"/"+depth3;
+//    }
 }
 
 
