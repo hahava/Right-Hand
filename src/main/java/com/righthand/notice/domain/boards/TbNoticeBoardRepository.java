4efe0ae74a9eb1498225b0fc25401d38f0e8c69f
SML <dltkdals2202@gmail.com>
2018-11-07 17:08:02 +0900

    feat : 공지사항 상세보기 api

--- src/main/java/com/righthand/notice/domain/boards/TbNoticeBoardRepository.java
+++ src/main/java/com/righthand/notice/domain/boards/TbNoticeBoardRepository.java
@@ -1,5 +1,14 @@
 package com.righthand.notice.domain.boards;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TbNoticeBoardRepository extends JpaRepository <TbNoticeBoard, Long>{}
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface TbNoticeBoardRepository extends JpaRepository <TbNoticeBoard, Long>{
+
+    @Query(value = "SELECT * FROM TB_NOTICE_BOARD t WHERE t.BOARD_SEQ = ?1", nativeQuery = true)
+    List<TbNoticeBoard> findByBoardSeq(long boardSeq);
+}
