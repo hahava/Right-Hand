98f6a591e853b3e88d76c63044124e6b0f7ab008
SML <dltkdals2202@gmail.com>
2018-11-13 17:00:01 +0900

    refactor : mypage @Query -> JPA Method
    findOne을 getOne으로 할 것!
    update는 @Modifying을 이용한 쿼리문을 작성!

--- src/main/java/com/righthand/mypage/info/TbUserRepository.java
+++ src/main/java/com/righthand/mypage/info/TbUserRepository.java
@@ -1,10 +1,7 @@
 package com.righthand.mypage.info;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface TbUserRepository extends JpaRepository<TbUser, Long> {
-
-    @Query(value = "SELECT t.EMAIL FROM TB_USER t WHERE USER_SEQ = ?1", nativeQuery = true)
-    String findEmail(int userSeq);
+     TbUser getOne(Long userSeq);
 }
