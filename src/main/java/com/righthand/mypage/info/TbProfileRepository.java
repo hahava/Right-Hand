05f48ca84a9f240bc4c5ffa50fe5b6ec8fe7796d
SML <dltkdals2202@gmail.com>
2018-11-13 10:48:18 +0900

    fix : 프로필 보기 API 수정

--- src/main/java/com/righthand/mypage/info/TbProfileRepository.java
+++ src/main/java/com/righthand/mypage/info/TbProfileRepository.java
@@ -4,17 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface TbProfileRepository extends JpaRepository<TbProfile, Long> {
 
-//    @Query(value = "SELECT t.USER_SEQ FROM TB_PROFILE t WHERE t.PROFILE_SEQ = ?1", nativeQuery = true)
-//    int findUserSeq(int profileSeq);
-
-    @Query(value = "SELECT t.USER_SEQ, t.GENDER, t.USER_NAME, t.BIRTH_YEAR, t.TEL FROM TB_PROFILE t WHERE t.PROFILE_SEQ = ?1", nativeQuery = true)
-    List<TbProfile> findUserSeq(int profileSeq);
+    @Query(value = "SELECT USER_SEQ, USER_NAME, GENDER, BIRTH_YEAR, TEL, PROFILE_SEQ FROM TB_PROFILE WHERE PROFILE_SEQ = ?1", nativeQuery = true)
+    TbProfile findUserSeq(int profileSeq);
 
     @Modifying
-    @Query(value = "UPDATE TB_PROFILE SET NICK_NAME = ?1 WHERE PROFILE_SEQ = ?2", nativeQuery = true)
+    @Query(value = "UPDATE TB_PROFILE SET t.NICK_NAME = ?1 WHERE PROFILE_SEQ = ?2", nativeQuery = true)
     int updateUserProfile(String nickname, int profileSeq);
 }
