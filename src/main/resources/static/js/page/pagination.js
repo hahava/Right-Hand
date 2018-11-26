3f262487ce40ae5d63dbbc7d4df275e26a2c313a
sj-kalin <hahava5696@gmail.com>
2018-11-26 17:42:13 +0900

    Feat : 회원활동기록 화면 및 기능 완료

--- src/main/resources/static/js/page/pagination.js
+++ src/main/resources/static/js/page/pagination.js
@@ -1,10 +1,15 @@
-function set_page(total, requested_page, requested_type) {
-    /*
-    *   totalpage= 전체 페이지 개수
-    *   전체 데이터가 41 개 라면, 가능한 페이지는 9
-    *   전체 데이터가 40 개 라면, 가능한 페이지는 8 이다.
-    *   stand를 수정하면 페이지 개수 수정이 가능하다.
-    * */
+/*
+*   totalpage= 전체 페이지 개수
+*   전체 데이터가 41 개 라면, 가능한 페이지는 9
+*   전체 데이터가 40 개 라면, 가능한 페이지는 8 이다.
+*   stand를 수정하면 페이지 개수 수정이 가능하다.
+* */
+function set_page(data, requested_page, addr) {
+
+
+    var address = setPageAddress(addr);
+
+    var total = data.total;
     var stand = 5;
     var totalPage = (total % 5 == 0) ? (total / 5) : (parseInt(total / 5) + 1);
     var startNum = parseInt((requested_page - 1) / stand) * stand + 1;
@@ -19,13 +24,14 @@ function set_page(total, requested_page, requested_type) {
     var nextArrow = ' <li id="nextArrow" ><a href="#" id="nextArrowAnc">»</a></li>';
     var li_tag = "";
 
-    for (var i = startNum; i <= endNum; i++) {
+    data = data.data;
+    for (var i = startNum, j = 0; i <= endNum; i++, j++) {
+
         //요청 페이지와 현재 페이지가 같다면 'active' 클래스를 입력한다.
         if (i == requested_page) {
             li_tag = li_tag + "<li class='active'><a >" + i + "</a></li>";
         } else {
-            li_tag = li_tag + "<li><a href='/board/list?type=" + requested_type + "&page=" + i + "'>" + i + "</a></li>";
-            // li_tag = li_tag + "<li><a onclick= 'req_page(" + i + ")'>" + i + "</a></li>";
+            li_tag = li_tag + "<li><a href=" + address + i + ">" + i + "</a></li>";
         }
     }
     $('#pageNation').append(prevArrow + li_tag + nextArrow);
@@ -35,7 +41,7 @@ function set_page(total, requested_page, requested_type) {
     } else {
         //5 이상일 경우 이전 버튼 활성화
         var prevCount = startNum - stand;
-        $('#prevArrowAnc').attr('onclick', 'req_page(' + prevCount + ')');
+        $('#prevArrowAnc').attr('href', address + prevCount);
     }
 
     // 마지막 페이지와 전체 페이지가 같으면 다음 버튼 비활성화
@@ -44,55 +50,25 @@ function set_page(total, requested_page, requested_type) {
     } else {
         // 다음 버튼 활성화
         var nextCount = startNum + stand;
-        $('#nextArrowAnc').attr('href', '/board/list?type=' + requested_type + '&page=' + nextCount);
+        $('#nextArrowAnc').attr('href', address + nextCount);
     }
 }
 
-function search_page(total, requested_page, requested_type, searchedWord) {
-    /*
-    *   totalpage= 전체 페이지 개수
-    *   전체 데이터가 41 개 라면, 가능한 페이지는 9
-    *   전체 데이터가 40 개 라면, 가능한 페이지는 8 이다.
-    *   stand를 수정하면 페이지 개수 수정이 가능하다.
-    * */
-    var stand = 5;
-    var totalPage = (total % 5 == 0) ? (total / 5) : (parseInt(total / 5) + 1);
-    var startNum = parseInt((requested_page - 1) / stand) * stand + 1;
-    var endNum;
-    if (startNum + (stand - 1) >= totalPage) {
-        endNum = totalPage;
-    } else {
-        endNum = startNum + (stand - 1);
-    }
 
-    var prevArrow = ' <li id="prevArrow" ><a href="#" id="prevArrowAnc">«</a></li> ';
-    var nextArrow = ' <li id="nextArrow" ><a href="#" id="nextArrowAnc">»</a></li>';
-    var li_tag = "";
-
-    for (var i = startNum; i <= endNum; i++) {
-        //요청 페이지와 현재 페이지가 같다면 'active' 클래스를 입력한다.
-        if (i == requested_page) {
-            li_tag = li_tag + "<li class='active'><a >" + i + "</a></li>";
-        } else {
-            li_tag = li_tag + "<li><a href='/board/search?type=" + requested_type + "&page=" + i + "&searchedWord=" + searchedWord + "'>" + i + "</a></li>";
+function setPageAddress(params_) {
+    var href = "";
+    for (var key in params_) {
+        if (key == 'default') {
+            href = params_[key] + '?';
+            continue;
+        }
+        if (typeof params_[key] !== "undefined") {
+            if (href.substr(href.length - 1, href.length) == '?') {
+                href = href + key + "=" + params_[key];
+            } else {
+                href = href + "&" + key + "=" + params_[key];
+            }
         }
     }
-    $('#pageNation').append(prevArrow + li_tag + nextArrow);
-    //5 이하일 경우 prev 버튼 클릭 금지
-    if (endNum <= stand) {
-        $('#prevArrow').attr('class', 'disabled');
-    } else {
-        //5 이상일 경우 이전 버튼 활성화
-        var prevCount = startNum - stand;
-        $('#prevArrowAnc').attr('onclick', 'req_page(' + prevCount + ')');
-    }
-
-    // 마지막 페이지와 전체 페이지가 같으면 다음 버튼 비활성화
-    if (endNum == totalPage) {
-        $('#nextArrow').attr('class', 'disabled');
-    } else {
-        // 다음 버튼 활성화
-        var nextCount = startNum + stand;
-        $('#nextArrowAnc').attr('href', '/board/search?type=' + requested_type + '&page=' + nextCount + "&searchedWord=" + searchedWord);
-    }
+    return href + "&page=";
 }
