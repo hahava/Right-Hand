70494515cdaf91c77b8667c145cacf80bd9e2a9e
sj-kalin <hahava5696@gmail.com>
2018-11-27 13:34:15 +0900

    Feat : userPwCheck js, html 분리

--- src/main/resources/static/js/component/navbar.js
+++ src/main/resources/static/js/component/navbar.js
@@ -7,10 +7,10 @@ function login_checker() {
             var auth = result.code;
             switch (auth) {
                 case 0:
-                    login_user();
+                    loginUser();
                     break;
                 default:
-                    not_login();
+                    notLogin();
             }
         }, error: function (e) {
 
@@ -18,14 +18,14 @@ function login_checker() {
     });
 }
 
-function login_user() {
-
+function loginUser() {
     $('#modalOpener').remove();
     $('#registerOpener').remove();
 
     $('#navbar').append('<li class="dropdown" id="myPage"><a href="#none" class="dropdown-toggle" data-toggle="dropdown">MyPage <span class="caret"></span></a>  ' +
         '<ul class="dropdown-menu dropdown-menu-left" role="menu">\n' +
         '<li><a href="/user/info">회원정보</a></li>\n' +
+        /* TODO : 추후 개발 예정 */
         // '<li><a href="image-gallery.html">토큰내역</a></li>\n' +
         '<li><a href="/user/activity">활동내역</a></li>\n' +
         '<li class="divider"></li>\n' +
@@ -33,7 +33,7 @@ function login_user() {
         '</ul></li>');
 }
 
-function not_login() {
+function notLogin() {
     $('#navbar').append('<li id="login_li"><a href="#" data-toggle="modal" data-target="#loginModal" id="modalOpener">Login</a>\n' +
         '                </li>\n' +
         '                <li id="register_li"><a href="#" data-toggle="modal" data-target="#registerModal" id="registerOpener">Register</a>\n' +
