a371af5f5e322a15749544283282f1e90a81551b
sj-kalin <hahava5696@gmail.com>
2018-11-27 11:45:33 +0900

    Fix : 회원가입 중복 체크

--- src/main/resources/static/js/component/login.js
+++ src/main/resources/static/js/component/login.js
@@ -1,16 +1,25 @@
-/* 로그인 성공 여부 확인 */
+/* 로그인 성공 여부 확인하여 모달 이벤트를 제어하는 변수 */
 var loginOk = false;
 
-/* 비동기 로그인 기능 */
 function login() {
+
+    var user_login_email = $('#user_login_email').val();
+    var user_login_pw = $('#user_login_pw').val();
+
+    // 아이디 또는 패스워드를 입력하지 않은 경우
+    if (user_login_email.length == 0 || user_login_pw.length == 0) {
+        alert("항목을 전부 채워주세요");
+        return;
+    }
+
     $.ajax({
         type: 'POST',
         url: "http://localhost:8080/api/login",
-        /* 비동기로 처리 시 리다이렉트 정상적으로 실행 안될 수 있음*/
+        /* 비동기로 처리 시 리다이렉트 정상적으로 실행 안될 수 있음 */
         async: false,
         data: {
-            userId: $('#user_login_email').val(),
-            userPwd: $('#user_login_pw').val()
+            userId: user_login_email,
+            userPwd: user_login_pw
         },
         success: function (data) {
             loginOk = true;
@@ -36,7 +45,15 @@ function logout() {
             location.reload();
         },
         error: function () {
+            alert("잘못된 접근입니다.")
         }
     });
 }
 
+// 모달이 켜질 때, 엔터키를 이벤트에 따란 submit 버튼을 클릭하게 됨
+$(document).keypress(function (e) {
+    // 로그인
+    if ($("#loginModal").hasClass('in') && (e.keycode == 13 || e.which == 13)) {
+        $('#login_submit').click();
+    }
+});
