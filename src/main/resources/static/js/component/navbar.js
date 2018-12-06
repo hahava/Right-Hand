e48f01b46f4688c808771f54d1ed461c8eccd1c3
sj-kalin <hahava5696@gmail.com>
2018-12-06 17:02:43 +0900

    Feat : nav 에 코인 보유량 html 삽입

--- src/main/resources/static/js/component/navbar.js
+++ src/main/resources/static/js/component/navbar.js
@@ -1,4 +1,7 @@
 function login_checker() {
+    var user_nickname;
+    var rhCoin;
+    var rewardPower;
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8080/api/membership/check/live/session',
@@ -7,7 +10,11 @@ function login_checker() {
             var auth = result.code;
             switch (auth) {
                 case 0:
-                    loginUser();
+                    user_nickname = result.data.nickName;
+                    rhCoin = result.data.rhCoin;
+                    rewardPower = result.data.rewardPower;
+                    var data = {'user_nickname': user_nickname, 'rhCoin': rhCoin, 'rewardPower': rewardPower};
+                    loginUser(data);
                     break;
                 default:
                     notLogin();
@@ -18,9 +25,13 @@ function login_checker() {
     });
 }
 
-function loginUser() {
+function loginUser(data) {
+
     $('#modalOpener').remove();
     $('#registerOpener').remove();
+
+    $('#navbar').append('<li><a href="#none">Coin <span class="badge">' + data.rhCoin + '</span></a></li>' +
+        '<li><a href="#none">Rp <span class="badge">' + data.rewardPower + '</span></a></li>');
 
     $('#navbar').append('<li class="dropdown" id="myPage"><a href="#none" class="dropdown-toggle" data-toggle="dropdown">MyPage <span class="caret"></span></a>  ' +
         '<ul class="dropdown-menu dropdown-menu-left" role="menu">\n' +
@@ -31,6 +42,8 @@ function loginUser() {
         '<li class="divider"></li>\n' +
         '<li><a href="javascript:logout();">LogOut</a></li>\n' +
         '</ul></li>');
+
+
 }
 
 function notLogin() {
