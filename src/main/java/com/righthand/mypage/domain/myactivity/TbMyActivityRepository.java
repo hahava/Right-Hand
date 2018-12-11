407a702a1e2e695bdde2f587185782415bd24b3d
SML <dltkdals2202@gmail.com>
2018-12-11 13:38:37 +0900

    feat : RH 파워 지급 내역 API 생성

--- src/main/java/com/righthand/mypage/domain/myactivity/TbMyActivityRepository.java
+++ src/main/java/com/righthand/mypage/domain/myactivity/TbMyActivityRepository.java
@@ -1,6 +1,10 @@
 package com.righthand.mypage.domain.myactivity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TbMyActivityRepository extends JpaRepository<TbMyActivity, Long> {
+    Page<TbMyActivity> findAllByActivityProfileSeq(Long id, Pageable pageable);
 }
