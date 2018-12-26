b91583ef2f727ed08a6482d1d03743f3e8af259d
SML <dltkdals2202@gmail.com>
2018-12-27 02:21:18 +0900

    refactor : modify .gitignore becauseof Configuration of SMTP

---
+++ src/main/java/com/righthand/membership/config/ConfigMail.java
@@ -0,0 +1,32 @@
+package com.righthand.membership.config;
+
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Data
+@Configuration
+public class ConfigMail {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("dltkdals2202@gmail.com");
+        mailSender.setPassword("smat9752");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
