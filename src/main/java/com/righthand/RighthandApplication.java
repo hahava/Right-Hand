c98562a3b0eba482cfd4b0ce3199e79a92901e2c
SML <dltkdals2202@gmail.com>
2020-01-13 23:22:17 +0900

    Application 수정

--- src/main/java/com/righthand/RighthandApplication.java
+++ src/main/java/com/righthand/RighthandApplication.java
@@ -2,6 +2,7 @@ package com.righthand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -13,8 +14,15 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableCaching
 public class RighthandApplication {
 
+	private static final String PROPERTIES = "spring.config.location=" +
+			"classpath:/application.yml" +
+			",classpath:/db.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(RighthandApplication.class, args);
+
+		new SpringApplicationBuilder(RighthandApplication.class)
+				.properties(PROPERTIES)
+				.build();
 	}
 }
 
